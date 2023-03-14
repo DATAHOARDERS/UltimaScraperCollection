e741d4ffdaa46cdce5bf20dd152b82cb79e7ecfb
CRIMINAL <89371864+DIGITALCRIMINALS@users.noreply.github.com>
2023-03-14 05:54:28 +0000

    v0.1.3

---
+++ test.py
@@ -0,0 +1,17 @@
+import asyncio
+
+import ultima_scraper_api
+
+from ultima_scraper_collection.managers.datascraper_manager.datascraper_manager import (
+    DataScraperManager,
+)
+
+
+async def main():
+
+    api = ultima_scraper_api.select_api("OnlyFans")
+    _datascraper = DataScraperManager().select_datascraper(api)
+    pass
+
+
+asyncio.run(main())
