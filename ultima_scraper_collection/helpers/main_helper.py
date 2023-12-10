26dd9b32594f82749c544630faab927171f69382
digitalcriminals <89371864+digitalcriminals@users.noreply.github.com>
2023-12-10 13:03:01 +0000

    Add space checking and file handling functions

--- ultima_scraper_collection/helpers/main_helper.py
+++ ultima_scraper_collection/helpers/main_helper.py
@@ -1,3 +1,5 @@
+import os
+from pathlib import Path
 from shutil import disk_usage
 from typing import Any
 
@@ -11,8 +13,6 @@ from ultima_scraper_collection.config import Directory
 
 def check_space(
     custom_directory: list[Directory],
-    min_size: int = 0,
-    priority: str = "download",
 ):
     root = ""
     while not root:
@@ -21,24 +21,21 @@ def check_space(
             # ISSUE
             # Could cause problems w/ relative/symbolic links that point to another hard drive
             # Haven't tested if it calculates hard A or relative/symbolic B's total space.
+            # size is in GB
             assert directory.path
             obj_Disk = disk_usage(str(directory.path.parent))
             free = obj_Disk.free / (1024.0**3)
             x = {}
             x["path"] = directory.path
             x["free"] = free
+            x["min_space"] = directory.minimum_space
             paths.append(x)
-        if priority == "download":
-            for item in paths:
-                download_path = item["path"]
-                free = item["free"]
-                if free > min_size:
-                    root = download_path
-                    break
-        elif priority == "upload":
-            paths.sort(key=lambda x: x["free"])
-            item = paths[0]
-            root = item["path"]
+        for item in paths:
+            download_path = item["path"]
+            free = item["free"]
+            if free > item["min_space"]:
+                root = download_path
+                break
     return root
 
 
@@ -55,10 +52,10 @@ async def is_valuable(user: DBUserModel | user_types):
         else:
             return False
     else:
-        if user.isPerformer:
+        if user.is_performer():
             if isinstance(user, OFUserModel):
                 if (
-                    user.subscribedIsExpiredNow == False
+                    user.subscribed_is_expired_now == False
                     or await user.get_paid_contents()
                 ):
                     return True
@@ -72,3 +69,35 @@ async def is_valuable(user: DBUserModel | user_types):
                     return False
         else:
             return False
+
+
+async def is_notif_valuable(api_user: user_types):
+    if await is_valuable(api_user):
+        if await api_user.subscription_price() == 0:
+            if isinstance(api_user, OFUserModel) and await api_user.get_paid_contents():
+                return True
+            return False
+        else:
+            return True
+    return False
+
+
+async def walk(directory: Path):
+    all_files: list[Path] = []
+    for root, _subdirs, files in os.walk(directory):
+        x = [Path(root, x) for x in files]
+        all_files.extend(x)
+    return all_files
+
+
+def find_unused_filename(filepath: Path):
+    base_name = filepath.stem  # Get the filename without extension
+    extension = filepath.suffix  # Get the file extension
+    counter = 2
+
+    while filepath.exists():
+        new_name = f"{base_name} ({counter}){extension}"
+        filepath = filepath.with_name(new_name)
+        counter += 1
+
+    return filepath
