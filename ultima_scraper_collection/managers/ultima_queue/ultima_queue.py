b65dee0b3f00a21ebe2bb533b0a1d224fbb042ac
digitalcriminals <89371864+digitalcriminals@users.noreply.github.com>
2024-12-07 05:42:27 +0000

    Added MandatoryJob class: Represents a job with attributes for job name, success queue, and failure queue. Supports serialization and deserialization via to_dict and from_dict methods.

--- ultima_scraper_collection/managers/ultima_queue/ultima_queue.py
+++ ultima_scraper_collection/managers/ultima_queue/ultima_queue.py
@@ -1,20 +1,197 @@
-from typing import Any, Callable
+from typing import Any, Callable, Coroutine
 
-from celery import Celery, Task
+import aio_pika
+import orjson
 
 
-class CustomTask(Task):
-    abstract = True  # Prevent direct instantiation
+class MandatoryJob:
+    def __init__(
+        self,
+        job_name: str,
+        success_queue_name: str | None = None,
+        failure_queue_name: str | None = None,
+    ) -> None:
+        self.job_name = job_name
+        self.success_queue_name = success_queue_name
+        self.failure_queue_name = failure_queue_name
 
-    def on_failure(self, exc, task_id, args, kwargs, einfo):
-        print(f"Task {task_id} failed with exception: {exc}. Retrying...")
-        self.retry(exc=exc, args=args)  # Retry on failure
+    def to_dict(self) -> dict[str, Any]:
+        return {
+            "job_name": self.job_name,
+            "success_queue_name": self.success_queue_name,
+            "failure_queue_name": self.failure_queue_name,
+        }
+
+    @classmethod
+    def from_dict(cls, data: dict[str, Any]) -> "MandatoryJob":
+        return cls(**data)
+
+
+class StandardData:
+    def __init__(
+        self,
+        site_name: str,
+        performer_id: int,
+        performer_username: str,
+        category: str = "",
+        mandatory_jobs: list[MandatoryJob] = [],
+        extra_data: dict[str, Any] = {},
+    ) -> None:
+        self.site_name = site_name
+        self.performer_id = performer_id
+        self.performer_username = performer_username
+        self.mandatory_jobs = mandatory_jobs
+        self.category = category
+        self.extra_data = extra_data
+
+    def to_dict(self) -> dict[str, Any]:
+        return {
+            "site_name": self.site_name,
+            "performer_id": self.performer_id,
+            "performer_username": self.performer_username,
+            "mandatory_jobs": [x.to_dict() for x in self.mandatory_jobs],
+            "category": self.category,
+            "extra_data": self.extra_data,
+        }
+
+    @classmethod
+    def from_dict(cls, data: dict[str, Any]) -> "StandardData":
+        mandatory_jobs = [
+            MandatoryJob.from_dict(x) for x in data.get("mandatory_jobs", [])
+        ]
+        data.pop("mandatory_jobs", None)
+        return cls(**data, mandatory_jobs=mandatory_jobs)
+
+    @classmethod
+    def from_legacy(cls, data: dict[str, Any]) -> "StandardData":
+        temp_data = data["data"]
+        return cls(
+            site_name=temp_data["site_name"],
+            performer_id=temp_data["performer_id"],
+            performer_username=temp_data["username"],
+        )
+
+    def create_mandatory_job(
+        self,
+        job_name: str,
+        success_queue_name: str | None = None,
+        failure_queue_name: str | None = None,
+    ) -> MandatoryJob:
+        mandatory_job = MandatoryJob(job_name, success_queue_name, failure_queue_name)
+        self.mandatory_jobs.append(mandatory_job)
+        return mandatory_job
 
 
 class UltimaQueue:
-    def __init__(self, name: str = "proj") -> None:
-        self.celery = Celery(
-            name,
-            broker="amqp://guest:guest@localhost:5672/",
+    def __init__(self, host: str = "localhost") -> None:
+        self.amqp_url = f"amqp://{host}"
+        self.connection = None
+        self.channel = None
+
+    def get_connection(self):
+        assert self.connection is not None
+        return self.connection
+
+    def get_channel(self):
+        assert self.channel is not None
+        return self.channel
+
+    async def connect(self, prefetch_count: int = 0):
+        if self.connection and not self.connection.is_closed:
+            return
+        self.connection = await aio_pika.connect_robust(self.amqp_url)
+        self.channel = await self.connection.channel()
+        await self.channel.set_qos(prefetch_count=prefetch_count)
+
+    async def disconnect(self):
+        if self.connection:
+            await self.connection.close()
+
+    async def declare_queue(self, queue_name: str, durable: bool = True):
+        if not self.channel or self.channel.is_closed:
+            await self.connect()
+        assert self.channel is not None
+        return await self.channel.declare_queue(
+            queue_name,
+            durable=durable,
+            arguments={"x-message-deduplication": True, "x-max-priority": 10},
         )
-        self.celery.conf.broker_connection_retry_on_startup = True
+
+    async def consume_messages(
+        self,
+        queue_name: str,
+        callback: Callable[[Any, Any], Coroutine[Any, Any, None]],
+        *args: Any,
+    ):
+        """Consume messages from a specified queue."""
+        if not self.channel or self.channel.is_closed:
+            await self.connect()
+
+        queue = await self.declare_queue(queue_name)
+
+        async with queue.iterator() as queue_iter:
+            async for message in queue_iter:
+                try:
+                    async with message.process(requeue=True, ignore_processed=True):
+                        await callback(message, *args)
+                except Exception as _e:
+                    continue
+        await self.disconnect()
+
+    async def publish_message(
+        self,
+        queue_name: str,
+        message: dict[str, Any],
+        durable: bool = True,
+        priority: int | None = None,
+        unique_id: str | None = None,
+        print_error: bool = True,
+    ):
+        if self.channel is None:
+            await self.connect()
+        await self.declare_queue(queue_name, durable)
+        assert self.channel is not None
+
+        if unique_id is None:
+            unique_id = message.get("id")
+        headers: dict[str, Any] = {}
+        if unique_id:
+            headers = {"x-deduplication-header": unique_id}
+        try:
+            await self.channel.default_exchange.publish(
+                aio_pika.Message(
+                    body=orjson.dumps(message),
+                    delivery_mode=(
+                        aio_pika.DeliveryMode.PERSISTENT
+                        if durable
+                        else aio_pika.DeliveryMode.NOT_PERSISTENT
+                    ),
+                    headers=headers,
+                    priority=priority,
+                ),
+                routing_key=queue_name,
+            )
+            print(f"Message published to {queue_name}")
+            return True
+        except aio_pika.exceptions.DeliveryError as e:
+            if print_error:
+                print(f"Error publishing message: {e}")
+            return False
+
+    async def publish_notification(self, message: dict[str, Any]):
+        await self.publish_message("telegram_notifications", message)
+        await self.publish_message("discord_notifications", message)
+
+    async def push_back_message(
+        self,
+        queue_name: str,
+        message: aio_pika.abc.AbstractIncomingMessage,
+        priority: int | None = None,
+    ):
+        await message.reject()
+        task = orjson.loads(message.body.decode())
+        await self.publish_message(queue_name, task, priority=priority)
+
+    async def close(self):
+        if self.connection:
+            await self.connection.close()
